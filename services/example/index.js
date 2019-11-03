37cd44ec7585678a32dd59ffdf09f6cafbf41d21
Brett <brett@hover.to>
2019-11-03 15:45:02 -0800

    parse data

--- services/example/index.js
+++ services/example/index.js
@@ -1,18 +1,8 @@
 'use strict'
 
-// module.exports = function (fastify, opts, next) {
-//   fastify.get('/example', function (request, reply) {
-//     reply.send('this is an example')
-//   })
-
-//   next()
-// }
-
-// If you prefer async/await, use the following
-
 module.exports = async function (fastify, opts) {
   fastify.get('/example', async function (request, reply) {
     // make the request to api to get schedule data
-    return 'this is just one an example'
+    return 'test ing this is just one an example'
   })
 }
